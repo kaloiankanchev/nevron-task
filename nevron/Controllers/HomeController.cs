7cf4af23595433f3c02f880f9385714119d324bd
Eliteware <Eliteware@ELITEMOBILE>
2024-02-25 23:08:40 +0200

    first commit

---
+++ nevron/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using nevron.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nevron.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const int MIN_NUMBER = 1;
+        private const int MAX_NUMBER = 100;
+        private const string NUMBERS = "Numbers";
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }            
+
+        [HttpPost("/clearNumbers")]
+        public IActionResult ClearNumbers()
+        {
+            Numbers = new List<int>();
+            return Ok();
+        }
+
+        [HttpPost("/addNumber")]
+        public IActionResult AddNumber()
+        {
+            var numbers = HttpContext.Session.Get<List<int>>(NUMBERS) ?? new List<int>();
+            var rand = new Random();
+            numbers.Add(rand.Next(MIN_NUMBER, MAX_NUMBER));
+            HttpContext.Session.Set(NUMBERS, numbers);
+            return Ok(numbers.Count());
+        }
+
+        [HttpGet("/sumNumbers")]
+        public IActionResult SumNumbers()
+        {
+            int sum = 0;
+            foreach (int num in Numbers)
+            {
+                sum += num;
+            }
+            return Ok(new { sum });
+        }
+
+        [HttpGet("/getNumbers")]
+        public IActionResult GetNumbers()
+        {
+            return Ok(new { numbers = Numbers });
+        }
+
+        private List<int> Numbers
+        {
+            get
+            {
+                if (HttpContext.Session.TryGetValue(NUMBERS, out byte[] value))
+                {
+                    return System.Text.Json.JsonSerializer.Deserialize<List<int>>(value);
+                }
+                else
+                {
+                    var numbers = new List<int>();
+                    HttpContext.Session.Set(NUMBERS, System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(numbers));
+                    return numbers;
+                }
+            }
+            set
+            {
+                HttpContext.Session.Set(NUMBERS, System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(value));
+            }
+        }
+    }
+}
---
+++ nevron/wwwroot/js/site.js
@@ -0,0 +1,53 @@
+﻿const numbersSumElement = document.getElementById('numbersSum');
+const numbersListElement = document.getElementById('numbersList');
+const counterElement = document.getElementById('counter');
+const notSumText = "Not summed";
+
+const clearNumberEndpoint = '/clearNumbers';
+const addNumberEndpoint = '/addNumber';
+const sumNumberEndpoint = '/sumNumbers';
+const getNumbersEndpoint = '/getNumbers';
+
+
+document.addEventListener('DOMContentLoaded', function () {  
+    updateNumbersList();
+});
+
+async function clearNumbers() {
+    await fetch(clearNumberEndpoint, { method: 'POST' });
+    updateNumbersList();
+    numbersSumElement.innerHTML = notSumText;
+}
+
+async function addNumber() {
+    await fetch(addNumberEndpoint, { method: 'POST' });
+    updateNumbersList();
+}
+
+async function sumNumbers() {
+    const response = await fetch(sumNumberEndpoint);
+    const data = await response.json();
+    if (data.sum === 0) {
+        numbersSumElement.innerText = notSumText;
+    }
+    else {
+        numbersSumElement.innerText = `${data.sum}`;
+    }
+}
+
+async function updateNumbersList() {
+    const response = await fetch(getNumbersEndpoint);
+    const data = await response.json();
+    numbersListElement.innerHTML = '&nbsp;';
+    data.numbers.forEach(number => {    
+        const listItem = document.createElement('span');
+        listItem.textContent = number;
+        listItem.style.marginRight = '5px';
+        listItem.style.background = 'lightgrey'; 
+        listItem.style.padding = '10px'; 
+        numbersListElement.appendChild(listItem);
+    });
+    counterElement.innerHTML = data.numbers.length;
+}
+
+
